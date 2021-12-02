b4a99da1cb8b5e9aeb1626033f7be7c71061eccb
oscarferm <79453808+oscarferm@users.noreply.github.com>
2021-12-02 10:40:16 +0100

    Oscar
    dd

---
+++ Assets/scripts/light.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light : MonoBehaviour
+{
+    Vector3 pos, dir;
+    GameObject lightObject;
+    LineRenderer light;
+    List<Vector3> lightindect = new List<Vector3>();
+
+    public light(Vector3 pos, Vector3 dir,Material material)
+    {
+        this.light = new LineRenderer();
+        this.lightObject = new GameObject;
+        this.lightObject.name = "light beam";
+        this.pos = pos;
+        this.dir = dir;
+
+        this.light = this.lightObject.AddComponent(typeof(LineRenderer)) as LineRenderer;
+        this.light.startWidth = 0.1f;
+        this.light.endWidth = 0.1f;
+        this.light.material = material;
+        this.light.startColor = Color.white;
+
+    }
+    void Start()
+    {
+     }
+
+   
+    void Update()
+    {
+        
+    }
+
+
+}
